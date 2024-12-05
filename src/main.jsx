829699bf36fd5189283525a65358826e5397b81e
Vishwas Kumar Verma <vishwasverma23@gmail.com>
2024-12-05 16:52:02 +0530

    updated sign up

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+import VerifyEmailPage from './components/VerifyEmailPage.jsx'
+import { RouterProvider } from 'react-router'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'VerifyEmailPage',
+    element:<VerifyEmailPage/>
+    
+ 
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ <RouterProvider router={router}></RouterProvider>
 )
